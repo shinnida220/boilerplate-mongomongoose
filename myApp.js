82d0fb65a21e81e815e40b3d489586b4717330d7
Nuel <ibk.com@gmail.com>
2022-03-30 11:39:55 +0100

    model findById

--- myApp.js
+++ myApp.js
@@ -64,7 +64,11 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) done(err);
+    if (data) done(null, data);
+  });
+  // done(null /*, data*/);
 };
 
 const findEditThenSave = (personId, done) => {
