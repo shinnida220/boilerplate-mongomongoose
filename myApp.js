76860409f637e7fc090bed61beb10ad571420ae0
Nuel <ibk.com@gmail.com>
2022-03-30 10:34:15 +0100

    added person schema

--- myApp.js
+++ myApp.js
@@ -3,6 +3,16 @@ mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Get the Schema
+const Schema = mongoose.Schema;
+
+// create a Person schema
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
 
 let Person;
 
