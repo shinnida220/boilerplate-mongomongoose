8f53a8ef7e60f30cd67f38d8b53ed8ab19a4393d
Nuel <ibk.com@gmail.com>
2022-03-30 12:43:17 +0100

    model findOneAndRemove

--- myApp.js
+++ myApp.js
@@ -96,7 +96,13 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, removedPerson) => {
+    if (err) done(err);
+    if (person) {
+      done(null, removedPerson);
+    }
+  });
+  // done(null /*, data*/);
 };
 
 const removeManyPeople = (done) => {
