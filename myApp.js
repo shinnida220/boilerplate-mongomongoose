1a4ef26e2d516bf5afcc3916997925aeae077418
Nuel <ibk.com@gmail.com>
2022-03-30 15:03:14 +0100

    model query chain sort asc

--- myApp.js
+++ myApp.js
@@ -131,7 +131,7 @@ const queryChain = (done) => {
   const foodToSearch = "burrito";
 
   Person.find({ favoriteFoods: foodToSearch })
-    .sort({ name: -1 })
+    .sort({ name: 1 })
     .limit(2)
     .select({ favoriteFoods: 1, name: 1 })
     .exec((err, people) => {
